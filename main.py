b0caeee9587adab46fa5c09a352f32a53714c5b8
Mika Mäki <mika.maki@kapsi.fi>
2017-12-08 10:51:47 +0200

    Move single run to a separate file

--- main.py
+++ main.py
@@ -1,49 +1,9 @@
 import matplotlib.pyplot as plt
 
-import color
-import locator
-import mapgrid
 import plotter
-import vehicle
 import logger
 log = logger.getLogger(__name__, level="DEBUG", disabled=False)  # Root logger
 import montecarlo
-
-
-def plot_single_run():
-    height = 10
-    width = 10
-
-    m = mapgrid.generate_map(width=width, height=height)
-    v = vehicle.Vehicle(m)
-
-    plot = plotter.LocatorPlot(v)
-    plot.plot_map()
-
-    # The placement of the vehicle need not be shown
-    # plot.plot_vehicle()
-    
-    log.info("Start location:", str(v.location()))
-    log.info("Start color:", v.color())
-
-    for i in range(6):
-        v.move_unbound()
-        plot.plot_vehicle()
-
-    # found, x, y = locator.locate(v.map(), v.history(), v)
-    num_matches, x, y = locator.locate(v.map(), v.history())
-    
-
-
-    log.info("End location:", v.location())
-    log.info("End color:", v.color())
-    if num_matches == 1:
-        loc_x, loc_y, dir = v.location()
-        log.info("Found on location", x, y, " and it is "+str((num_matches==1) and (x==loc_x) and (y==loc_y)))
-    else:
-        log.warning("Location not found!")
-    
-    plot.show()
 
 
 def plot_monte_carlos(num_runs=20, iterations=1000, use_stored_results=[True,True]):
@@ -80,14 +40,11 @@ def plot_monte_carlos(num_runs=20, iterations=1000, use_stored_results=[True,Tru
     
     plt.show()
 
+
 def main():
-    #plot_single_run()
     plot_monte_carlos(num_runs=10, iterations=3000, use_stored_results=[True, True])
     #montecarlo.log_stuff()
     #montecarlo.debug_my_stuff()
     
 
-    
 main()
-#plot_single_run()
-
---
+++ single_run.py
@@ -0,0 +1,49 @@
+import logger
+import locator
+import mapgrid
+import vehicle
+import plotter
+
+log = logger.getLogger(__name__, level="DEBUG", disabled=False)  # Root logger
+
+
+def plot_single_run():
+    height = 10
+    width = 10
+
+    m = mapgrid.generate_map(width=width, height=height)
+    v = vehicle.Vehicle(m)
+
+    plot = plotter.LocatorPlot(v)
+    plot.plot_map()
+
+    # The placement of the vehicle need not be shown
+    # plot.plot_vehicle()
+
+    log.info("Start location:", str(v.location()))
+    log.info("Start color:", v.color())
+
+    for i in range(6):
+        v.move_unbound()
+        plot.plot_vehicle()
+
+    # found, x, y = locator.locate(v.map(), v.history(), v)
+    num_matches, x, y = locator.locate(v.map(), v.history())
+
+    log.info("End location:", v.location())
+    log.info("End color:", v.color())
+    if num_matches == 1:
+        loc_x, loc_y, dir = v.location()
+        log.info(
+            "Found on location",
+            x, y,
+            " and its correctness is " + str((num_matches == 1) and (x == loc_x) and (y == loc_y))
+        )
+    else:
+        log.warning("Location not found!")
+
+    plot.show()
+
+
+if __name__ == "__main__":
+    plot_single_run()
